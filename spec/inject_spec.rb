fb1c85bd1d284c2c7082cced2bc21b5ad210a61b
Oliver <o.lucas@live.com>
2015-01-02 18:40:16 +0000

    Updated Tests

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -23,11 +23,49 @@ describe Array do
     it 'can add numbers together'
       expect([1, 2, 3, 4].inject(:+)).to eq(10)
       expect([1, 2, 3, 4].my_inject(:+)).to eq(10)
+    end
 
+    it 'can minus numbers' do
+      expect([1, 2, 3, 4].inject (:-)).to eq -8
+      expect([1, 2, 3, 4].my_inject (:-)).to eq -8
+    end
+
+    it 'can multiply numbers' do
+      expect([1, 2, 3, 4].inject (:*)).to eq 24
+      expect([1, 2, 3, 4].my_inject(:*)).to eq 24
+    end
   end
 
   context 'with an argument and block' do
+    it 'can add numbers together' do
+      expect([1, 2, 3, 4].inject(10){|memo, item| memo + item}).to eq(20)
+      expect([1, 2, 3, 4].my_inject(10){|memo, item| memo + item}).to eq(20)
+    end
+
+    it 'can minus numbers' do
+      expect([1, 2, 3, 4].inject(5){|memo, item| memo - item}).to eq(-5)
+      expect([1, 2, 3, 4].my_inject(5){|memo, item| memo - item}).to eq(-5)
+    end
+
+    it 'can multiply numbers' do
+      expect([1, 2, 3, 4].inject(10){|memo, item| memo * item}).to eq(240)
+      expect([1, 2, 3, 4].my_inject(10){|memo, item| memo * item}).to eq(240)
+    end
   end
 
   context 'with a symbol argument' do
+    it 'can add with a starting point and a symbol' do
+      expect([1,2,3,4].inject(10, :+)).to eq 20
+      expect([1,2,3,4].my_inject(10, :+)).to eq 20
+    end
+
+    it 'can minus with a starting point and a symbol' do
+      expect([1, 2, 3, 4]).inject(10, :-).to eq 0
+      expect([1, 2, 3, 4]).my_inject(10, :-).to eq 0
+    end
+
+    it 'can multiply with a starting point and a symbol' do
+      expect([1, 2, 3, 4]).inject(10, :*).to eq 240
+      expect([1, 2, 3, 4]).my_inject(10, :*).to eq 240
+    end
   end
